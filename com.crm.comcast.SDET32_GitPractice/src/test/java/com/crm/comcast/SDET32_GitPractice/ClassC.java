c128d2511ddb0e6c36014461fb1fd3f1f0d8c5d6
Ross-07 <95274458+Ross-07@users.noreply.github.com>
2022-04-12 17:07:00 +0530

    Update ClassC.java

--- com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
+++ com.crm.comcast.SDET32_GitPractice/src/test/java/com/crm/comcast/SDET32_GitPractice/ClassC.java
@@ -11,6 +11,8 @@ public class ClassC {
         System.out.println("Six Push");
 		System.out.println("Seventh Push");	
 		System.out.println("Eigth Push");
+		System.out.println("hi sitesh this is RK making some change in the code pls check if the  change has worked or not");
+	        System.out.println("Jai Hind Jai Bharath");
 	}
 
 }
